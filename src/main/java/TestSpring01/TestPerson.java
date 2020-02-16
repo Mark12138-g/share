3edb93a1d9579d065b123fc5508b2212e2223b1a
mark12138-g <gys976192423@163.com>
2020-02-16 16:26:22 +0800

    Revert "v0.5"
    This reverts commit a63f3a802554dc805d841ef298b51b92b0c0592d.

--- src/main/java/TestSpring01/TestPerson.java
+++ src/main/java/TestSpring01/TestPerson.java
@@ -17,6 +17,7 @@ public class TestPerson {
     @Scope("prototype")
     @Bean(initMethod = "init",destroyMethod = "destory")
     public  Student student(){
-        return  new Student(20000,"sb");
+        return  new Student(10000,"sb");
     }
+
 }
